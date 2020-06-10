ab47fe46cca8cd29382825483c2b5f83e7be8f40
Menaures <robinmarcel.vogt@gmail.com>
2020-06-10 10:16:39 +0200

    added entire directory tree

--- uebungen/uebungsblatt-04/uebungsblatt-04.py
+++ uebungen/uebungsblatt-04/uebungsblatt-04.py
@@ -1,58 +1,87 @@
 """
-Simulation of a PT1-Element in form of an inverting op-amp circuit with
-negative feedback over a capacitor.
-
-u(t) = v_E(t):          input voltage
-x(t) = v_C(t):          capacitor voltage
-y(t) = x(t) = v_A(t):   output voltage
-
+Copyright Jonas Bilal, Robin Vogt
+Sources: Systemtheorie file toolbox_sr1
 """
 
-import sys
-import SuR.uebungen.toolbox_sr1 as sr1
 import numpy as np
-import scipy.integrate as integrate
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+import toolbox_sr1 as sr
 
-sys.path.append("D://Uni/SuR")
-
-
-def f(x, u):
-    # parameters
-    R_1 = 100  # [Ohm]
-    R_2 = 900  # [Ohm]
-    C = 1e-3  # [F]
-
-    # ODE
-    xdot = -1/(R_2*C)*x -1/(R_1*C)*u
-
+def ode_f(x, u):
+    """
+    Implements the ode of an negative OpAmp with 
+    a capacity.
+    """
+    R1 = 100  # 100 Ohm
+    R2 = 900  # 900 Ohm
+    C = 10 ** -3  # 3 mF
+    xdot = np.array([-1 / (R2 * C) * x[0] - 1 / (R1 * C) * u])
     return xdot
 
 
-def y(x, u):
-    return x
+def output_g(x, u):
+    return x[0]
 
 
-# simulation parameters
-T_f = 5  # [s]
-delta_t = 0.0005  # [s]
-num_steps = int(T_f / delta_t)
+def simulation_constant_input():
+    x0 = np.array([0])
+    stepTime = 0.01
+    numSteps = 500
+    U = np.linspace(1, 1, numSteps)
+    return sr.nlsim(ode_f, x0, U, stepTime, output_g)
 
-# input control
-U = np.linspace(1, 1, num_steps)
 
-# initial conditions
-x_0 = 0
+def simulation_sin_input(w):
+    f = w / (2 * np.pi) # Hz
+    u = w*(np.linspace(0, 4*np.pi/w, 1000))
+    U = np.sin(w*(np.linspace(0, 4 * np.pi / w, 1000)))
+    x0 = np.array([0])
+    stepTime = 4*np.pi/(w*1000)
+    return [sr.nlsim(ode_f, x0, U, stepTime, output_g), u, U]
 
-# linear simulation
-[X, Y, T] = sr1.nlsim(f, x_0, U, delta_t, y)
 
-# plot
-plt.figure(1)
-plt.plot(T, Y)
-plt.step(T[: -1], U, color='red')
-plt.title('Integrator non-linear simulation')
-plt.xlabel('t [s]')
-plt.ylabel('Voltage [V]')
-plt.legend(["$v_A(t)$", "$v_E(t)$"])
-plt.savefig("plot.png")
+def main():
+    plt.subplot(221)
+    [X1, Y1, T1] = simulation_constant_input()
+    plt.plot(T1, np.linspace(1, 1, 501), label = "Eingangsspannung")
+    plt.plot(T1, Y1, label = "Ausgangsspannung", color = "green")
+    plt.xlabel("$t$ in s")
+    plt.ylabel("$v_C(t)$ in V")
+    plt.title("Simulation der Ausgangsspannung $v_C(t)$ des invertierenden Verstärkers")
+    plt.legend()
+    plt.grid(True)
+
+    plt.subplot(222)
+    [X2, Y2, T2] = simulation_sin_input(0.1)[0]
+    plt.plot(T2, np.sin(0.1 * T2), label = "Eingangssignal")
+    plt.plot(T2, Y2, label = "Ausgangssignal", color = "red")
+    plt.legend()
+    plt.xlabel("$t$ in s")
+    plt.ylabel("$v_C(t)$ in V")
+    #plt.xlim(0,3 * 0.1 / (2*np.pi))
+    plt.title("Simulation von Eingangs- und Ausgangsspannung bei $\omega = 0.1$ rad/s")
+    plt.grid(True)
+
+    plt.subplot(223)
+    [X3, Y3, T3] = simulation_sin_input(10)[0]
+    plt.plot(T3, Y3, label = "Ausgangssignal", color = "purple")
+    plt.plot(T3, np.sin(10 * T3), label = "Eingangssignal")
+    plt.legend()
+    plt.xlabel("$t$ in s")
+    plt.ylabel("$v_C(t)$ in V")
+    plt.title("Simulation von Eingangs- und Ausgangsspannung bei $\omega = 10$ rad/s")
+    plt.grid(True)
+
+    plt.subplot(224)
+    [X4, Y4, T4] = simulation_sin_input(100)[0]
+    plt.plot(T4, Y4, label = "Ausgangssignal")
+    plt.plot(T4, np.sin(100 * T4), label = "Eingangssignal")
+    plt.xlabel("$t$ in s")
+    plt.ylabel("$v_C(t)$ in V")
+    plt.title("Simulation von Eingangs- und Ausgangsspannung bei $\omega = 100$ rad/s")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+if __name__ == "__main__":
+   main() 
---
+++ uebungen/uebungsblatt-04/uebungsblatt-04_own.py
@@ -0,0 +1,58 @@
+"""
+Simulation of a PT1-Element in form of an inverting op-amp circuit with
+negative feedback over a capacitor.
+
+u(t) = v_E(t):          input voltage
+x(t) = v_C(t):          capacitor voltage
+y(t) = x(t) = v_A(t):   output voltage
+
+"""
+
+import sys
+import SuR.uebungen.toolbox_sr1 as sr1
+import numpy as np
+import scipy.integrate as integrate
+import matplotlib.pyplot as plt
+
+sys.path.append("D://Uni/SuR")
+
+
+def f(x, u):
+    # parameters
+    R_1 = 100  # [Ohm]
+    R_2 = 900  # [Ohm]
+    C = 1e-3  # [F]
+
+    # ODE
+    xdot = -1/(R_2*C)*x -1/(R_1*C)*u
+
+    return xdot
+
+
+def y(x, u):
+    return x
+
+
+# simulation parameters
+T_f = 5  # [s]
+delta_t = 0.0005  # [s]
+num_steps = int(T_f / delta_t)
+
+# input control
+U = np.linspace(1, 1, num_steps)
+
+# initial conditions
+x_0 = 0
+
+# linear simulation
+[X, Y, T] = sr1.nlsim(f, x_0, U, delta_t, y)
+
+# plot
+plt.figure(1)
+plt.plot(T, Y)
+plt.step(T[: -1], U, color='red')
+plt.title('Integrator non-linear simulation')
+plt.xlabel('t [s]')
+plt.ylabel('Voltage [V]')
+plt.legend(["$v_A(t)$", "$v_E(t)$"])
+plt.savefig("plot.png")
