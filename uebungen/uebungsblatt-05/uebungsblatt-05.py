af6de7b7acbe149ceb63d1c4d68a36ff0595148e
Menaures <robinmarcel.vogt@gmail.com>
2020-06-12 02:24:50 +0200

    "added all plots"

--- uebungen/uebungsblatt-05/uebungsblatt-05.py
+++ uebungen/uebungsblatt-05/uebungsblatt-05.py
@@ -17,7 +17,7 @@ def ode_f1(x, u):
 
     """
     A = np.array([[0, 1],
-                 [-1, -2]])
+                  [-1, -2]])
     B = np.array([0, 1]).T
 
     xdot = np.dot(A, x) + np.dot(B, u)
@@ -29,7 +29,7 @@ def ode_f2(x, u):
 
     """
     A = np.array([[0, 1],
-                 [-1, -1]])
+                  [-1, -1]])
     B = np.array([0, 1]).T
 
     xdot = np.dot(A, x) + np.dot(B, u)
@@ -41,7 +41,7 @@ def ode_f3(x, u):
 
     """
     A = np.array([[0, 1],
-                 [-1, -4]])
+                  [-1, -4]])
     B = np.array([0, 1]).T
 
     xdot = np.dot(A, x) + np.dot(B, u)
@@ -53,7 +53,7 @@ def ode_f4(x, u):
 
     """
     A = np.array([[0, 1],
-                 [-1/4, -1/2]])
+                  [-1 / 4, -1 / 2]])
     B = np.array([0, 1]).T
 
     xdot = np.dot(A, x) + np.dot(B, u)
@@ -72,15 +72,17 @@ def output_g(ODE, u):
     return ODE[0]
 
 
-def control_sim(x, u):
+def control_sim():
     A = np.array([[-4, 3, -3],
                   [-1, -1, -2],
                   [-2, 4, -4]])
-    B = np.array([1, 0, 2]).T
+    B = np.array([[1],
+                  [0],
+                  [2]])
     C = np.array([3, 1, 0])
     D = np.array([0])
 
-    ctl.ss(A, B, C, D)
+    return ctl.ss(A, B, C, D)
 
 
 def main():
@@ -100,7 +102,6 @@ def main():
     plt.plot(T1, Y1, label="Funktion 1", color="green")
     plt.xlabel("$t$ in s")
     plt.ylabel("$y(t)")
-    plt.title("Verzögerungsglieder zweiter Ordnung")
     plt.legend()
     plt.grid(True)
 
@@ -109,7 +110,6 @@ def main():
     plt.plot(T2, Y2, label="Funktion 2", color="blue")
     plt.xlabel("$t$ in s")
     plt.ylabel("$y(t)")
-    plt.title("Verzögerungsglieder zweiter Ordnung")
     plt.legend()
     plt.grid(True)
 
@@ -118,7 +118,6 @@ def main():
     plt.plot(T3, Y3, label="Funktion 3", color="red")
     plt.xlabel("$t$ in s")
     plt.ylabel("$y(t)")
-    plt.title("Verzögerungsglieder zweiter Ordnung")
     plt.legend()
     plt.grid(True)
 
@@ -141,26 +140,113 @@ def main():
     plt.legend()
     plt.grid(True)
 
-    # simulation parameter for slow motion
-    step_time = 0.1  # in sec
-    sim_time = 20  # in sec
-    num_steps = int(sim_time / step_time)
-    x_0 = np.array([0, 0])
-    U = np.array(np.linspace(1, 1, num_steps))
-
     # plot
     ODE = ode_f2
     [X5, Y5, T5] = sr1.nlsim(ODE, x_0, U, step_time, output_g)
-    T5 = np.linspace(1, 20, int(num_steps/2))
-    plt.plot(T5, Y5[:int((len(Y5)-1)/2)], label="Funktion 2 verlangsamt",
+    T5 = np.linspace(1, 20, int(num_steps / 2))
+    plt.plot(T5, Y5[:int((len(Y5) - 1) / 2)], label="Funktion 2 verlangsamt",
              color="blue")
     plt.xlabel("$t$ in s")
     plt.ylabel("$y(t)$")
     plt.legend()
     plt.grid(True)
 
-    # plt.show()
     plt.savefig("plots/plots_1g.png")
+    plt.clf()
+
+    # control simulation
+    sys1 = control_sim()
+
+    # step response
+    [T6, Y6] = ctl.step_response(sys1)
+    plt.figure()
+    plt.plot(T6, Y6, label="Sprungantwort")
+    plt.xlabel("$t$ in s")
+    plt.ylabel("$y(t)$")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("plots/plot_2b.png")
+    plt.clf()
+
+    # calculate t_settling
+    dc_gain = Y6[-1]
+    for i in range(len(Y6)):
+        if Y6[i] <= dc_gain + abs(0.02 * dc_gain):
+            print(T6[i])
+            break
+
+    # initial response
+    X_0 = [1, -1, 1]
+    [T7, Y7] = ctl.initial_response(sys1, X0=X_0)
+    plt.figure()
+    plt.plot(T7, Y7, label="Antwortfunktion")
+    plt.xlabel("$t$ in s")
+    plt.ylabel("$y(t)$")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("plots/plot_2c.png")
+
+    # forced response
+    # Rectangular wave signal
+    sim_time = 5
+    num_steps = 2000
+    omega1 = 0.2 * np.pi
+    U = np.multiply(np.sign(np.sin(2 * np.pi * omega1 * np.linspace(0,
+                                                                     sim_time,
+                                                                     num_steps))),
+                     0.5)
+    T8 = np.linspace(0, sim_time, num_steps)
+    [T8, Y8, X8] = ctl.forced_response(sys1, T=T8, U=U)
+    plt.figure()
+    plt.plot(T8, Y8, label="Ausgang $y(t)$")
+    plt.xlabel("$t$ in s")
+    plt.ylabel("$y(t)$")
+    plt.plot(T8, U, label="Eingang $u(t)$")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("plots/plot_2d_0.1.png")
+    plt.clf()
+
+    # forced response
+    # Rectangular wave signal
+    sim_time = 0.5
+    num_steps = 2000
+    omega1 = 2 * np.pi
+    U = np.multiply(np.sign(np.sin(2 * np.pi * omega1 * np.linspace(0,
+                                                                     sim_time,
+                                                                     num_steps))),
+                     0.5)
+    T9 = np.linspace(0, sim_time, num_steps)
+    [T9, Y9, X9] = ctl.forced_response(sys1, T=T9, U=U)
+    plt.figure()
+    plt.plot(T9, Y9, label="Ausgang $y(t)$")
+    plt.xlabel("$t$ in s")
+    plt.ylabel("$y(t)$")
+    plt.plot(T9, U, label="Eingang $u(t)$")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("plots/plot_2d_1.png")
+    plt.clf()
+
+    # forced response
+    # Rectangular wave signal
+    sim_time = 0.05
+    num_steps = 2000
+    omega1 = 20 * np.pi
+    U = np.multiply(np.sign(np.sin(2 * np.pi * omega1 * np.linspace(0,
+                                                                     sim_time,
+                                                                     num_steps))),
+                     0.5)
+    T10 = np.linspace(0, sim_time, num_steps)
+    [T10, Y10, X10] = ctl.forced_response(sys1, T=T10, U=U)
+    plt.figure()
+    plt.plot(T10, Y10, label="Ausgang $y(t)$")
+    plt.xlabel("$t$ in s")
+    plt.ylabel("$y(t)$")
+    plt.plot(T10, U, label="Eingang $u(t)$")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("plots/plot_2d_10.png")
     plt.clf()
 
 
